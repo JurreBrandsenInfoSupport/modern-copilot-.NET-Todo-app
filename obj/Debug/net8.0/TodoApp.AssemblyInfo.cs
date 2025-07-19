e0ef4889a7b8458d485be5627f6545f24018ef8b
Jurre Brandsen <jurre.brandsen@infosupport.com>
2025-07-19 17:46:42 +0200

    ignore file

--- obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea70403a2ada215bebafeac7d09667553a337c0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
